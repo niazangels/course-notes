eeaacb6d3cb04f763afc47487598461aa7f7821b
Niyas Mohammed <niaz.angels@gmail.com>
2019-12-26 01:50:29 +0530

    Move variables to config.py

--- deployment-of-machine-learning-models/resources/exercise-6/regression_model/regression_model/config/config.py
+++ deployment-of-machine-learning-models/resources/exercise-6/regression_model/regression_model/config/config.py
@@ -41,3 +41,43 @@ FEATURES = [
     # this one is only to calculate temporal variable:
     "YrSold",
 ]
+
+# categorical variables with NA in train set
+CATEGORICAL_VARS_WITH_NA = [
+    "MasVnrType",
+    "BsmtQual",
+    "BsmtExposure",
+    "FireplaceQu",
+    "GarageType",
+    "GarageFinish",
+]
+
+TEMPORAL_VARS = ["YearRemodAdd"]
+TEMPORAL_REFERENCE_VARS = "YrSold"
+
+# this variable is to calculate the temporal variable,
+# can be dropped afterwards
+DROP_FEATURES = ["YrSold"]
+
+# variables to log transform
+NUMERICALS_LOG_VARS = ["LotFrontage", "1stFlrSF", "GrLivArea"]
+
+# numerical variables with NA in train set
+NUMERICAL_VARS_WITH_NA = ["LotFrontage"]
+
+# categorical variables to encode
+CATEGORICAL_VARS = [
+    "MSZoning",
+    "Neighborhood",
+    "RoofStyle",
+    "MasVnrType",
+    "BsmtQual",
+    "BsmtExposure",
+    "HeatingQC",
+    "CentralAir",
+    "KitchenQual",
+    "FireplaceQu",
+    "GarageType",
+    "GarageFinish",
+    "PavedDrive",
+]
--- deployment-of-machine-learning-models/resources/exercise-6/regression_model/regression_model/pipeline.py
+++ deployment-of-machine-learning-models/resources/exercise-6/regression_model/regression_model/pipeline.py
@@ -2,68 +2,39 @@ from sklearn.linear_model import Lasso
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import MinMaxScaler
 from regression_model import preprocessors as pp
+from regression_model.config import config
 
-
-# categorical variables with NA in train set
-CATEGORICAL_VARS_WITH_NA = [
-    "MasVnrType",
-    "BsmtQual",
-    "BsmtExposure",
-    "FireplaceQu",
-    "GarageType",
-    "GarageFinish",
-]
-
-TEMPORAL_VARS = ["YearRemodAdd"]
-TEMPORAL_REFERENCE_VARS = "YrSold"
-
-# this variable is to calculate the temporal variable,
-# can be dropped afterwards
-DROP_FEATURES = ["YrSold"]
-
-# variables to log transform
-NUMERICALS_LOG_VARS = ["LotFrontage", "1stFlrSF", "GrLivArea"]
-
-# numerical variables with NA in train set
-NUMERICAL_VARS_WITH_NA = ["LotFrontage"]
-
-# categorical variables to encode
-CATEGORICAL_VARS = [
-    "MSZoning",
-    "Neighborhood",
-    "RoofStyle",
-    "MasVnrType",
-    "BsmtQual",
-    "BsmtExposure",
-    "HeatingQC",
-    "CentralAir",
-    "KitchenQual",
-    "FireplaceQu",
-    "GarageType",
-    "GarageFinish",
-    "PavedDrive",
-]
 
 price_pipe = Pipeline(
     [
         (
             "categorical_imputer",
-            pp.CategoricalImputer(variables=CATEGORICAL_VARS_WITH_NA),
+            pp.CategoricalImputer(variables=config.CATEGORICAL_VARS_WITH_NA),
         ),
-        ("numerical_inputer", pp.NumericalImputer(variables=NUMERICAL_VARS_WITH_NA)),
+        (
+            "numerical_inputer",
+            pp.NumericalImputer(variables=config.NUMERICAL_VARS_WITH_NA),
+        ),
         (
             "temporal_variable",
             pp.TemporalVariableEstimator(
-                variables=TEMPORAL_VARS, reference_variable=TEMPORAL_REFERENCE_VARS
+                variables=config.TEMPORAL_VARS,
+                reference_variable=config.TEMPORAL_REFERENCE_VARS,
             ),
         ),
         (
             "rare_label_encoder",
-            pp.RareLabelCategoricalEncoder(tol=0.01, variables=CATEGORICAL_VARS),
+            pp.RareLabelCategoricalEncoder(tol=0.01, variables=config.CATEGORICAL_VARS),
         ),
-        ("categorical_encoder", pp.CategoricalEncoder(variables=CATEGORICAL_VARS)),
-        ("log_transformer", pp.LogTransformer(variables=NUMERICALS_LOG_VARS)),
-        ("drop_features", pp.DropUnecessaryFeatures(variables_to_drop=DROP_FEATURES)),
+        (
+            "categorical_encoder",
+            pp.CategoricalEncoder(variables=config.CATEGORICAL_VARS),
+        ),
+        ("log_transformer", pp.LogTransformer(variables=config.NUMERICALS_LOG_VARS)),
+        (
+            "drop_features",
+            pp.DropUnecessaryFeatures(variables_to_drop=config.DROP_FEATURES),
+        ),
         ("scaler", MinMaxScaler()),
         ("Linear_model", Lasso(alpha=0.005, random_state=0)),
     ]
