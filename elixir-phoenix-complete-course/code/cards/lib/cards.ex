1fe5b3f2368837ae6c74af92990cff888024a56f
Niyas Mohammed <niaz.angels@gmail.com>
2021-02-12 15:23:35 +0530

    Updates

---
+++ elixir-phoenix-complete-course/code/cards/lib/cards.ex
@@ -0,0 +1,27 @@
+defmodule Cards do
+  @moduledoc """
+  Documentation for `Cards`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Cards.hello()
+      :world
+
+  """
+  def hello do
+    "hi there"
+    # :world
+  end
+
+  def create_deck do
+    ["Ace", "Two", "Three"]
+  end
+
+  def shuffle(deck) do
+    
+  end
+end
